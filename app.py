a47134c9d0e8e8eb12207621d5b7665e7852d877
elledee003 <lavaletherealtor@gmail.com>
2025-01-27 22:13:28 -0500

    app.py

--- app.py
+++ app.py
@@ -21,8 +21,9 @@ config = load_config()
 DEXSCREENER_API_URL = config["dex_api_url"]
 RUGCHECK_API_URL = config["rugcheck_api_url"]
 DATABASE_URL = config["database_url"]
-TELEGRAM_BOT_TOKEN = config["telegram_bot_token"]
-TELEGRAM_CHAT_ID = config["telegram_chat_id"]
+TELEGRAM_BOT_TOKEN = config["7269899424:AAEGC6BKEE8BU-aJCx01OnbdcVfb7y00h1I
+"]
+TELEGRAM_CHAT_ID = config["TheRealJackPotTheBot"]
 BONKBOT_COMMAND_PREFIX = config["bonkbot_command_prefix"]
 
 # Initialize Telegram bot
